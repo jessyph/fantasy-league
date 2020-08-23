c7f435956f0ef00a6c464c209f21b44f69a3a304
Jessy Parker Humphreys <Jessy@jessicas-air.home>
2020-08-23 14:03:52 +0100

    add points tally

--- xp.R
+++ xp.R
@@ -1,3 +1,8 @@
 library(tidyverse)
+library(dplyr)
 
 xgdata <- read_csv("https://raw.githubusercontent.com/nandy47/Premier-League-Data-19-20/master/PLMetaData.csv")
+
+fantasyLeagueAttack <- xgdata %>%
+  filter(grepl('F|M', position)) %>% # Filter for attacking players
+  mutate(points = as.numeric((as.numeric(goals) * 3) + (as.numeric(assists) * 2))) # Creates new column based on points tally
