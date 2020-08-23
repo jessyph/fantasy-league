f40b42fbe48629e5692ec71d896413c20fd19be7
Jessy Parker Humphreys <Jessy@jessicas-air.home>
2020-08-23 19:54:08 +0100

    creating xP

--- xp.R
+++ xp.R
@@ -5,4 +5,6 @@ xgdata <- read_csv("https://raw.githubusercontent.com/nandy47/Premier-League-Dat
 
 fantasyLeagueAttack <- xgdata %>%
   filter(grepl('F|M', position)) %>% # Filter for attacking players
-  mutate(points = as.numeric((as.numeric(goals) * 3) + (as.numeric(assists) * 2))) # Creates new column based on points tally
+  mutate(points = as.numeric((as.numeric(goals) * 3) + (as.numeric(assists) * 2))) %>% # Creates new column based on points tally
+  mutate_at(vars(xG, xA), funs(round(., 2))) %>% # Round xG 
+  mutate(xP = as.numeric((as.numeric(xG) * 3) + (as.numeric(xA) * 2))) 
