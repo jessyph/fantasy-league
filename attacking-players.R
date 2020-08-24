2df9ca934f26274da590d6ad716ece330d35c56e
Jessy Parker Humphreys <Jessy@jessicas-air.home>
2020-08-24 19:16:10 +0100

    nice font

--- attacking-players.R
+++ attacking-players.R
@@ -1,23 +1,27 @@
 library(ggplot2)
 library(ggrepel)
+library(showtext)
 
+font_add_google("Roboto Condensed", "roboto")
 
-ggplot(data = fantasyLeagueAttack, aes(x = points, y = xP)) +
+showtext_auto()
+
+ggplot(data = fantasyLeagueAttack %>% filter(time > 1100), aes(x = points, y = xP)) +
   geom_point(colour = "#817A7A") + 
   stat_smooth(method = "lm", se = FALSE, colour = "black") +
   geom_text_repel(data = fantasyLeagueAttack %>%
                     filter(xP >= 40) %>%
                     filter(points >= 40),
                   aes(label = player_name),
-                  colour = "#0AA21C") +
+                  family = "roboto") +
   geom_text_repel(data = fantasyLeagueAttack %>%
                     filter(xP < 40) %>%
                     filter (xP >= 20) %>%
                     filter(points >= 20),
                   aes(label = player_name),
-                  colour = "#F07D11") +
+                  family = "roboto") +
   geom_text_repel(data = fantasyLeagueAttack %>%
                     filter(xP >= 40) %>%
                     filter(points < 40),
                   aes(label = player_name),
-                  colour = "#F03611")
+                  family = "roboto")
