f1f9283dcff0be37a75fe513ecbb2e25b983ffa1
Jessy Parker Humphreys <Jessy@jessicas-air.home>
2020-08-24 09:29:53 +0100

    move graph

--- xp.R
+++ xp.R
@@ -12,22 +12,3 @@ fantasyLeagueAttack <- xgdata %>%
   mutate_at(vars(xG, xA), funs(round(., 2))) %>% # Round xG 
   mutate(xP = as.numeric((as.numeric(xG) * 3) + (as.numeric(xA) * 2))) 
 
-ggplot(data = fantasyLeagueAttack, aes(x = points, y = xP)) +
-  geom_point(colour = "#817A7A") + 
-  stat_smooth(method = "lm", se = FALSE, colour = "black") +
-  geom_text_repel(data = fantasyLeagueAttack %>%
-              filter(xP >= 40) %>%
-                filter(points >= 40),
-            aes(label = player_name),
-            colour = "#0AA21C") +
-  geom_text_repel(data = fantasyLeagueAttack %>%
-                    filter(xP < 40) %>%
-                    filter (xP >= 20) %>%
-                    filter(points >= 20),
-                  aes(label = player_name),
-                  colour = "#F07D11") +
-  geom_text_repel(data = fantasyLeagueAttack %>%
-                    filter(xP >= 40) %>%
-                    filter(points < 40),
-                  aes(label = player_name),
-                  colour = "#F03611")
